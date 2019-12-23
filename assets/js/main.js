15d9d30ad08c91f22384500971ccd40bd9ebb48d
Abdelrahman Kazamel <50080379+Kazamel@users.noreply.github.com>
2019-12-24 01:12:53 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        rtl: true,
+        prevArrow: '.prev',
+        nextArrow: '.next',
+        // variableWidth: false
+        // centerPadding: '1'
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.client-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        rtl: true,
+        prevArrow: '.prev-client',
+        nextArrow: '.next-client',
+        // variableWidth: false,
+        // centerPadding: '10%',
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
+
+
+// MY-CARSOUL
+$num = $('.my-card').length;
+$even = $num / 2;
+$odd = ($num + 1) / 2;
+
+if ($num % 2 == 0) {
+    $('.my-card:nth-child(' + $even + ')').addClass('active');
+    $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
+    $('.my-card:nth-child(' + $even + ')').next().addClass('next');
+} else {
+    $('.my-card:nth-child(' + $odd + ')').addClass('active');
+    $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
+    $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
+}
+
+$('.my-card').click(function () {
+    $slide = $('.active').width();
+    console.log($('.active').position().left);
+
+    if ($(this).hasClass('next')) {
+        $('.card-carousel').stop(false, true).animate({ left: '-=' + $slide });
+    } else if ($(this).hasClass('prev')) {
+        $('.card-carousel').stop(false, true).animate({ left: '+=' + $slide });
+    }
+
+    $(this).removeClass('prev next');
+    $(this).siblings().removeClass('prev active next');
+
+    $(this).addClass('active');
+    $(this).prev().addClass('prev');
+    $(this).next().addClass('next');
+});
+
+
+// Keyboard nav
+$('html body').keydown(function (e) {
+    if (e.keyCode == 37) { // left
+        $('.active').prev().trigger('click');
+    }
+    else if (e.keyCode == 39) { // right
+        $('.active').next().trigger('click');
+    }
+});
